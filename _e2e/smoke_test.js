de18d96065bbe5a15093be632b64c90d64cd44b0
hinata <yy.hinata33@gmail.com>
2024-12-14 21:05:08 +0900

    E2Eテストツールのインストール

---
+++ _e2e/smoke_test.js
@@ -0,0 +1,15 @@
+SuiteOf('スモークテスト');
+
+Scenario("example.comにアクセスする", ({ I }) => {
+  I.amOnPage("https://example.com");
+  I.see("Example Domain");
+});
+
+Scenario('Webサイトを開きログインする', ({ I }) => {
+  I.amOnPage("/"); // BASE_URLからの相対パスに書き換える
+  I.click("ログインする");
+  I.fillField("ユーザー名", "user1");
+  I.fillField("パスワード", "super-strong-passphrase");
+  I.click("ログイン");
+  I.see("user1 さん");
+})
--- e2e/codecept.conf.js
+++ e2e/codecept.conf.js
@@ -5,34 +5,22 @@ setHeadlessWhen(process.env.HEADLESS);
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
 setCommonPlugins();
-require('dotenv').config(); // dotenvを読み込む
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: "./*_test.js",
-  output: "./output",
+  tests: './*_test.js',
+  output: './output',
   helpers: {
     Playwright: {
-      url: process.env.BASE_URL, // process.env.BASE_URL に環境変数が入っている
+      url: 'http://localhost',
       show: true,
-      browser: "chromium",
-    },
+      browser: 'chromium'
+    }
   },
   include: {
-    I: "./steps_file.js",
+    I: './steps_file.js'
   },
-  translation: "en-US",
-  vocabularies: ["./vocabularies.json"],
-  name: "e2e",
-  plugins: {
-    allure: {
-      enabled: true,
-      require: "allure-codeceptjs",
-    },
-    stepByStepReport: {
-      enabled: true,
-      screenshotsForAllureReport: true,
-      deleteSuccessful: false,
-    },
-  },
-};
+  translation: 'en-US',
+  vocabularies: ['./vocabularies.json'],
+  name: 'e2e'
+}
